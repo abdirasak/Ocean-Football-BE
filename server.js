ad2eede8e1d2059d782e72fb9ef46f4b7a4c536e
abdirasak <abdi_2020@hotmail.co.uk>
2023-06-05 19:43:26 +0100

    Created error handling middleware

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 const connectDB = require('./config/db')
 const cors = require('cors');
+const { errorHandler } = require("./middleware/errorMiddleware")
 connectDB()
 
 
@@ -12,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use('/api/articles', require('./routes/articleRoute'))
+
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
